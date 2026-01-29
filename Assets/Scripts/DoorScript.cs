0473a84f8c2c7d373a21009d3b03c1df0e4067c0
Renyi03 <yinyeji@gmail.com>
2026-01-29 10:43:07 +0100

    Merge branch 'merge' into Yin

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoorScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject PopUpText;
+    //public GameObject parent;
+
+    bool playerInside;
+    bool popUP;
+
     void Start()
     {
-        
+        PopUpText.SetActive(false);
+        popUP = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        PopUpText.SetActive(popUP);
+
+        if (!playerInside) return;
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Oasis");
+        }
+    }
+
+    void Interact()
+    {
+        Debug.Log("Interacted with object");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        Debug.Log("Entered");
+        playerInside = true;
+        popUP = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+
+        playerInside = false;
+        popUP = false;
     }
 }
